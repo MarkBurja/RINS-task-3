fb34e82ca0f7013841bb0343af7287c71a440ae0
MatevzVidovic <matevz.vidovic@gmail.com>
2024-06-04 16:42:09 +0200

    State setting 3-5. In task3noRC launch.

---
+++ launch/task3noRC.launch.py
@@ -0,0 +1,31 @@
+import launch
+import launch_ros.actions
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        launch_ros.actions.Node(
+            package="dis_tutorial7",
+            executable="arm_mover_actions.py",
+            name="arm_mover_actions"
+        ),
+        launch_ros.actions.Node(
+            package="RINS-task-3",
+            executable="image_gatherer.py",
+            name="image_gatherer"
+        ),
+        launch_ros.actions.Node(
+            package="RINS-task-3",
+            executable="cylinder_segmentation",
+            name="cylinder_segmentation"
+        ),
+        launch_ros.actions.Node(
+            package="RINS-task-3",
+            executable="detect_people.py",
+            name="detect_people"
+        ),
+        launch_ros.actions.Node(
+            package="RINS-task-3",
+            executable="detect_rings.py",
+            name="detect_rings"
+        )
+    ])
